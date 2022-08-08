6c855c44e2159e496b05e784424344e6615844e1
chaeeun-Han <becky2525@naver.com>
2022-08-09 00:24:17 +0900

    make html, js, css

---
+++ routes/page.js
@@ -0,0 +1,31 @@
+// !!!!!!!! 경     고 !!!!!!!!!!!! 이 파일 수정하지 마세요~ -채은-
+
+const express = require('express');
+
+const router = express.Router();
+
+// 사용자 이름, 아이디, 비밀번호 요청 및 응답
+router.use((req, res, next) => {
+    res.locals.name = null;
+    res.locals.id = null;
+    res.locals.password = null;
+    next(); 
+});
+
+router.get('/profile', (req, res) => {
+    res.render('profile', { title: '내 정보'});
+});
+
+router.get('/join', (req, res) => {
+    res.render('join', {title: '회원가입'});
+});
+
+router.get('/', (req, res, next) => {
+    const twits = [];
+    res.render('index', {
+        title: 'planD',
+        twits,
+    });
+});
+
+module.exports = router;
