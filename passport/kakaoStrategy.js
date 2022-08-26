3135be61e06ec24ca427436b11a7ecab071d6e3b
chaeeun-Han <becky2525@naver.com>
2022-08-26 17:21:49 +0900

    kakao login fin

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -16,11 +16,23 @@ module.exports = () => {
             if (exUser) {
                 done(null, exUser);
             } else {
+                const birth = profile._json.kakao_account.birthday;
+                const month = birth.slice(0,2);
+                const date = birth.slice(2);
+                var genkor = "성별";
+                if (profile._json.kakao_account.gender == "female")
+                    genkor = "여자";
+                else
+                    genkor = "남자";
+
                 const newUser = await User.create({
-                    email: profile._json && profile._json.kakao_account_email,
-                    nick: profile.displayName,
-                    id: profile.id,
+                    email: profile._json.kakao_account.email,
+                    name: profile.displayName,
+                    // id: profile.id,
                     provider: 'kakao',
+                    birthday: `${month}-${date}`,
+                    gender: genkor,
+                    password: "kakao-password"
                 });
                 done(null, newUser);
             }
--- routes/auth.js
+++ routes/auth.js
@@ -59,7 +59,7 @@ router.get('/kakao', passport.authenticate('kakao'));
 router.get('/kakao/callback', passport.authenticate('kakao', {
     failureRedirect: '/',
 }), (req, res) => {
-    res.redirect('/');
+    res.redirect('/report');
 });
 
 module.exports = router;
